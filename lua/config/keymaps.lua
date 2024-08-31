f3ac7949e896458b001cb19f4fd433e130347f73
kevincal1226 <kevincal@umich.edu>
2024-08-31 11:03:17 -0400

    <leader>T for new tab window

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,4 +8,10 @@ require("telescope").load_extension("file_browser")
 -- Define the custom mapping
 vim.keymap.set("n", "<leader><space>", ":Telescope file_browser path=%:p:h=%:p:h<cr>", { desc = "Browse files" })
 
+vim.keymap.set(
+    "n",
+    "<leader>T",
+    ":tabnew | term<CR>",
+    { desc = "Open New Terminal Tab", noremap = true, silent = true }
+)
 -- Load the file browser extension
