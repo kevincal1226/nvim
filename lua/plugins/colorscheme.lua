2aa08a22d67a5cb382ca20d3338f3370bc968caf
Kevin Calopisis <kevincal@umich.edu>
2024-09-12 22:31:20 -0400

    E

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -14,3 +14,50 @@ return {
         vim.cmd("colorscheme flow")
     end,
 }
+
+-- Your existing LazyVim configuration...
+
+-- ~/.config/nvim/lua/plugins.lua or similar
+-- ~/.config/nvim/lua/lazyvim/plugins.lua
+
+-- ~/.config/nvim/lua/lazyvim/plugins.lua
+
+-- ~/.config/nvim/lua/lazyvim/plugins.lua
+
+-- ~/.config/nvim/lua/lazyvim/plugins.lua
+
+--return {
+--    -- Other plugins ...
+--
+--    "lancewilhelm/horizon-extended.nvim",
+--    lazy = false,
+--    priority = 1000,
+--    config = function()
+--        require("horizon-extended").setup({
+--            style = "neo",
+--            transparent = false,
+--            terminal_colors = true,
+--            enable_italics = false,
+--            show_end_of_buffer = false,
+--            underline = false,
+--            undercurl = true,
+--            styles = {
+--                booleans = { italic = false, bold = false },
+--                comments = { italic = false, bold = false },
+--                conditionals = { italic = false, bold = false },
+--                delimiters = { italic = false, bold = false },
+--                functions = { italic = false, bold = false },
+--                keywords = { italic = false, bold = false },
+--                loops = { italic = false, bold = false },
+--                operators = { italic = false, bold = false },
+--                properties = { italic = false, bold = false },
+--                strings = { italic = false, bold = false },
+--                types = { italic = false, bold = false },
+--                variables = { italic = false, bold = false },
+--            },
+--        })
+--        vim.cmd("colorscheme horizon-extended")
+--    end,
+--}
+--
+--
