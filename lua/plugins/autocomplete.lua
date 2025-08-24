8902997daa917266c9a411f7412c9e7405bc0b8c
kevincal1226 <kevincal@umich.edu>
2025-08-24 13:58:09 -0400

    update autocomplete for new lazyvim updates

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,7 +1,19 @@
+-- return {
+--     "saghen/blink.cmp",
+--     opts = {
+--         -- keymap = { preset = "super-tab" },
+--         -- other configurations...
+--     },
+-- }
+
+-- ~/.config/nvim/lua/plugins/blink.lua
 return {
     "saghen/blink.cmp",
     opts = {
-        keymap = { preset = "super-tab" },
-        -- other configurations...
+        keymap = {
+            preset = "default",
+            -- Override Tab behavior
+            ["<Tab>"] = { "accept", "fallback" },
+        },
     },
 }
